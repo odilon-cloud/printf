cde1cd8a80b467fa0afad533268d7faa38d724dc
odilon-cloud <oirambna@gmail.com>
2022-04-19 23:17:06 +0000

    commit

--- format-specifiers.c
+++ format-specifiers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_char - Prints character
  * @list: list of arguments
@@ -9,7 +9,7 @@ int print_char(va_list list)
 	_write_char(va_arg(list, int));
 	return (1);
 }
- 
+
 /**
  * print_string - Prints a string
  * @list: list of arguments
@@ -19,7 +19,7 @@ int print_string(va_list list)
 {
 	int i;
 	char *str;
- 
+
 	str = va_arg(list, char *);
 	if (str == NULL)
 		str = "(null)";
@@ -27,7 +27,7 @@ int print_string(va_list list)
 		_write_char(str[i]);
 	return (i);
 }
- 
+
 /**
  * print_percent - Prints a percent symbol
  * @list: list of arguments
@@ -38,7 +38,7 @@ int print_percent(__attribute__((unused))va_list list)
 	_write_char('%');
 	return (1);
 }
- 
+
 /**
  * print_integer - Prints an integer
  * @list: list of arguments
@@ -47,11 +47,10 @@ int print_percent(__attribute__((unused))va_list list)
 int print_integer(va_list list)
 {
 	int num_length;
- 
+
 	num_length = print_number(list);
 	return (num_length);
 }
- 
 /**
  * unsigned_integer - Prints Unsigned integers
  * @list: List of all of the argumets
@@ -60,12 +59,12 @@ int print_integer(va_list list)
 int unsigned_integer(va_list list)
 {
 	unsigned int num;
- 
+
 	num = va_arg(list, unsigned int);
- 
+
 	if (num == 0)
 		return (print_unsgined_number(num));
- 
+
 	if (num < 1)
 		return (-1);
 	return (print_unsgined_number(num));

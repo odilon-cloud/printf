70b670b943f3a96fc350387f65cb6a375680a570
odilon-cloud <oirambna@gmail.com>
2022-04-19 22:03:48 +0000

    main.h

--- main.h
+++ main.h
@@ -22,6 +22,17 @@ typedef struct convert conver_t;
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
 int _write_char(char c);
-
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
 
 #endif
